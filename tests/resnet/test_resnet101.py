9dc211ae71e010da76a675a1fe3f87112f72cea8
Alexander Pivovarov <apivovarov@gmail.com>
2023-01-23 22:46:48 -0800

    Update test_resnet101.py
    Fix Typo: ResNet50 -> ResNet101

--- tests/resnet/test_resnet101.py
+++ tests/resnet/test_resnet101.py
@@ -10,9 +10,9 @@ def test_output_softmax():
     key = jax.random.PRNGKey(0)
     x = jax.random.uniform(key, shape=(1, 224, 224, 3), minval=0, maxval=1)
 
-    resnet50 = fm.ResNet50(output='softmax', pretrained=None)
-    params = resnet50.init(key, x)
-    out = resnet50.apply(params, x, train=False)
+    resnet101 = fm.ResNet101(output='softmax', pretrained=None)
+    params = resnet101.init(key, x)
+    out = resnet101.apply(params, x, train=False)
 
     assert jnp.min(out) >= 0.0 and jnp.max(out) <= 1.0
 
@@ -22,9 +22,9 @@ def test_output_activations():
     key = jax.random.PRNGKey(0)
     x = jax.random.uniform(key, shape=(1, 224, 224, 3), minval=0, maxval=1)
 
-    resnet50 = fm.ResNet50(output='activations', pretrained=None)
-    params = resnet50.init(key, x)
-    out = resnet50.apply(params, x, train=False)
+    resnet101 = fm.ResNet101(output='activations', pretrained=None)
+    params = resnet101.init(key, x)
+    out = resnet101.apply(params, x, train=False)
 
     assert isinstance(out, dict)
 
@@ -36,12 +36,11 @@ def test_reference_output():
     x = jnp.array(img, dtype=jnp.float32) / 255.0
     x = jnp.expand_dims(x, axis=0)
 
-    resnet50 = fm.ResNet50(output='logits', pretrained='imagenet')
-    params = resnet50.init(key, x)
-    out = resnet50.apply(params, x, train=False)
+    resnet101 = fm.ResNet101(output='logits', pretrained='imagenet')
+    params = resnet101.init(key, x)
+    out = resnet101.apply(params, x, train=False)
     
-    out_ref = jnp.load('tests/resnet/aux_files/resnet50_elefant_output_ref.npy')
+    out_ref = jnp.load('tests/resnet/aux_files/resnet101_elefant_output_ref.npy')
     diff = jnp.mean(jnp.abs(out - out_ref))
 
     assert diff < 1e-5
-
